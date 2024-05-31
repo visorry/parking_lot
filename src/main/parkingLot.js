795c62a263b0be22d562dabf37ecdbd320f4546d
Vishnu R <vishnuvisa00@gmail.com>
2024-05-31 20:00:09 +0530

    Implemented createParkingLot method in ParkingLot class

--- src/main/parkingLot.js
+++ src/main/parkingLot.js
@@ -5,6 +5,13 @@ class ParkingLot {
     this.capacity = 0;
     this.slots = [];
   }
+
+  createParkingLot(capacity) {
+    this.capacity = capacity;
+    this.slots = Array(capacity).fill(null);
+    return `Created a parking lot with ${capacity} slots`;
+  }
+  
 }
 
 module.exports = ParkingLot;
