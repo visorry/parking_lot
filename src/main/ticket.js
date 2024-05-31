c54fd4438fd24597ca5e4aa22ee8388aa3c49c7a
Vishnu R <vishnuvisa00@gmail.com>
2024-05-31 19:51:02 +0530

    Implemented ticket class

--- src/main/ticket.js
+++ src/main/ticket.js
@@ -1,6 +1,10 @@
 class Ticket {
-    
-  }
+    constructor(slotNumber, registrationNumber, color) {
+        this.slotNumber = slotNumber;
+        this.registrationNumber = registrationNumber;
+        this.color = color;
+    }
+}
   
-  module.exports = Ticket;
+module.exports = Ticket;
   
