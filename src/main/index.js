0bfcbb98efdb598d72fe8d6437637b959e11f0ec
Vishnu R <vishnuvisa00@gmail.com>
2024-06-01 21:26:37 +0530

    Add interactive mode to main entry point

--- src/main/index.js
+++ src/main/index.js
@@ -3,3 +3,16 @@ const readline = require('readline');
 const CommandExecutor = require('./commands');
 
 const commandExecutor = new CommandExecutor();
+
+if (process.argv.length > 2) {
+    // file input mode
+    const fileStream = fs.createReadStream(process.argv[2]);
+    const rl = readline.createInterface({
+      input: fileStream,
+      crlfDelay: Infinity
+    });
+  
+    rl.on('line', (line) => {
+      console.log(commandExecutor.execute(line));
+    });
+  }
