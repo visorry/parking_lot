da7139b1a53e94c80c224d5fd089f15a30c9d90c
Vishnu R <vishnuvisa00@gmail.com>
2024-06-01 21:04:42 +0530

    Implement slot_numbers_for_cars_with_colour command in CommandExecutor

--- src/main/commands.js
+++ src/main/commands.js
@@ -18,6 +18,8 @@ class CommandExecutor {
         return this.parkingLot.status();
       case 'registration_numbers_for_cars_with_colour':
         return this.parkingLot.registrationNumbersForCarsWithColour(args[0]);
+      case 'slot_numbers_for_cars_with_colour':
+        return this.parkingLot.slotNumberForCarsWithColour(args[0]);
       default:
         return 'Invalid command';
     }
