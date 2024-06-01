c1e76938040813e6c10a95b184ac2385dff2ddd7
Vishnu R <vishnuvisa00@gmail.com>
2024-06-01 20:55:17 +0530

    Implement create_parking_lot command in CommandExecutor

--- src/main/commands.js
+++ src/main/commands.js
@@ -4,6 +4,16 @@ class CommandExecutor {
   constructor() {
     this.parkingLot = new ParkingLot();
   }
+
+  execute(commandLine) {
+    const [command, ...args] = commandLine.split(' ');
+    switch (command) {
+      case 'create_parking_lot':
+        return this.parkingLot.createParkingLot(parseInt(args[0]));
+      default:
+        return 'Invalid command';
+    }
+  }
 }
 
 module.exports = CommandExecutor;
