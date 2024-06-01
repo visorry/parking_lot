3b3beaf5a7cc3b2e86a2d6a28def685299c779aa
Vishnu R <vishnuvisa00@gmail.com>
2024-06-01 20:59:03 +0530

    Implement park command in CommandExecutor

--- src/main/commands.js
+++ src/main/commands.js
@@ -10,6 +10,8 @@ class CommandExecutor {
     switch (command) {
       case 'create_parking_lot':
         return this.parkingLot.createParkingLot(parseInt(args[0]));
+      case 'park':
+        return this.parkingLot.park(args[0], args[1]);
       default:
         return 'Invalid command';
     }
